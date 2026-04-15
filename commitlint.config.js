b0c4c94340f1c19ee59dfba53826c7f1cd27c87e
dudemaz <maznicenkomisa@gmail.com>
2026-04-15 21:24:14 +0300

    feat: добавление проверки коммитов

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+       'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build', 'ci', 'wip']],
+        'scope-case': [2, 'always', 'lower-case'],
+        'subject-case': [2, 'always', 'lower-case'],
+        'subject-full-stop': [2, 'never', '.'],
+        'header-max-length': [2, 'always', 100],
+    },
+};
